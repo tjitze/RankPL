123a9a83e8713ee98378612fe3e2607622806bde
Tjitze <tjitze@gmail.com>
2017-04-05 23:50:41 +0200

    Improve RankPL command line utility

--- src/com/tr/rp/RankPL.java
+++ src/com/tr/rp/RankPL.java
@@ -21,16 +21,34 @@ import com.tr.rp.parser.DefProgParser;
 public class RankPL {
 
 	public static void main(String[] args) {
+		
+		// Process options
 		if (args.length < 2) {
-			System.err.println("Usage: java -jar RPL.jar source_file <max_rank> [var1] [var2] ...");
+			printUsage();
 			System.exit(-1);
 		}
 		int maxRank = 0;
 		try {
 			maxRank = Integer.parseInt(args[1]);
 		} catch (Exception e) {
-			System.err.println("Usage: java -jar RPL.jar <source_file> <max_rank> <var1> <var2> ...");
+			printUsage();
+			System.exit(-1);
 		}
+		String[] variables = Arrays.copyOfRange(args, 2, args.length);
+		for (String var: variables) {
+			if (var.length() == 0 || !Character.isAlphabetic(var.charAt(0))) {
+				System.err.println("Invalid variable name: " + var);
+				System.exit(-1);
+			}
+			for (char c: var.toCharArray()) {
+				if (!Character.isAlphabetic(c) && !Character.isDigit(c)) {
+					System.err.println("Invalid variable name: " + var);
+					System.exit(-1);
+				}
+			}
+		}
+
+		// Parse input
         File file = new File(args[0]);
         FileInputStream fis = null;
         ANTLRInputStream input = null;
@@ -46,13 +64,33 @@ public class RankPL {
         ConcreteParser classVisitor = new ConcreteParser();
         DStatement program = (DStatement)classVisitor.visit(parser.program());
 		RankedIterator it = program.getIterator(new InitialVarStoreIterator());
-		if (args.length > 2) {
-			it = new MarginalizingIterator(it, Arrays.copyOfRange(args, 2, args.length));
+
+		// Apply variable list
+		if (variables.length > 0) {
+			it = new MarginalizingIterator(it, variables);
 		}
+		
+		// Print outcomes
+		boolean emptyResult = false;
 		while (it.next() && it.getRank() <= maxRank) {
 			VarStore v = it.getVarStore();
+			if (v.toString().equals("[]")) {
+				emptyResult = true;
+			}
 			int rank = it.getRank();
 			System.out.println("Rank " + rank + ": " + v);
 		}
+		if (emptyResult) {
+			System.out.println("Warning: one or more outcomes did not assign a value to any of the variables " + Arrays.toString(variables) + ".");
+		}
 	}
+
+	private static void printUsage() {
+		System.out.println("Usage: java -jar RankPL.jar <source_file> <max_rank> [var1] [var2] ...");
+		System.out.println();
+		System.out.println("  where: <source_file>       is the RankPL source file to execute");
+		System.out.println("         <max_rank>          is the maximum rank to show (i.e. only outcomes ranked at most <max_rank> are shown)");
+		System.out.println("         [var1] [var2] ...   are the variables to show (if none are specified then all variables are shown)");
+		System.out.println();
+	}	
 }

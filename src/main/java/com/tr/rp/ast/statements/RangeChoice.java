2b4ec808d4ee0b5a7a2b3f16707a75c7332f6138
Tjitze <tjitze@gmail.com>
2017-06-07 22:56:44 +0200

    Clean up imports

--- src/main/java/com/tr/rp/ast/statements/RangeChoice.java
+++ src/main/java/com/tr/rp/ast/statements/RangeChoice.java
@@ -1,23 +1,17 @@
 package com.tr.rp.ast.statements;
 
-import java.util.ArrayList;
 import java.util.Set;
-import java.util.List;
 
 import com.tr.rp.ast.AbstractExpression;
 import com.tr.rp.ast.AbstractStatement;
 import com.tr.rp.ast.LanguageElement;
-import com.tr.rp.ast.expressions.AbstractFunctionCall;
 import com.tr.rp.ast.expressions.AssignmentTarget;
-import com.tr.rp.ast.expressions.FunctionCall;
 import com.tr.rp.ast.expressions.Literal;
-import com.tr.rp.ast.expressions.Variable;
 import com.tr.rp.ast.statements.FunctionCallForm.ExtractedExpression;
 import com.tr.rp.exceptions.RPLException;
 import com.tr.rp.iterators.ranked.ExecutionContext;
 import com.tr.rp.iterators.ranked.RankTransformIterator;
 import com.tr.rp.iterators.ranked.RankedIterator;
-import com.tr.rp.tools.Pair;
 import com.tr.rp.varstore.VarStore;
 
 /**

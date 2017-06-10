1ccfaa763f8b1e804776fb95a4c99467846a9d20
Tjitze <tjitze@gmail.com>
2017-06-10 02:00:08 +0200

    Clean up imports

--- src/test/java/com/tr/rp/rpl/RunRPLTest.java
+++ src/test/java/com/tr/rp/rpl/RunRPLTest.java
@@ -1,9 +1,7 @@
 package com.tr.rp.rpl;
 
 import java.io.IOException;
-import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;

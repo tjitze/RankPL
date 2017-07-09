b0d3118e908ca25d53b61b0d5638289a4a7bb7db
Tjitze <tjitze@gmail.com>
2017-07-09 14:13:00 +0200

    Add push/pop/peek stack operations to ast

---
+++ src/main/java/com/tr/rp/ast/expressions/Peek.java
@@ -0,0 +1,80 @@
+package com.tr.rp.ast.expressions;
+
+import java.util.Set;
+
+import com.tr.rp.ast.AbstractExpression;
+import com.tr.rp.ast.LanguageElement;
+import com.tr.rp.exceptions.RPLException;
+import com.tr.rp.varstore.VarStore;
+import com.tr.rp.varstore.types.Type;
+
+/**
+ * Peek top of stack.
+ */
+public class Peek extends AbstractExpression {
+
+	private final AbstractExpression e;
+	
+	public Peek(AbstractExpression e) {
+		this.e = e;
+	}
+
+	@Override
+	public void getVariables(Set<String> list) {
+		e.getVariables(list);
+	}
+
+	@Override
+	public LanguageElement replaceVariable(String a, String b) {
+		return new Peek((AbstractExpression)e.replaceVariable(a, b));
+	}
+
+	@Override
+	public boolean hasRankExpression() {
+		return e.hasRankExpression();
+	}
+
+	@Override
+	public AbstractExpression transformRankExpressions(VarStore v, int rank) throws RPLException {
+		return new Peek(e.transformRankExpressions(v, rank));
+	}
+
+	@Override
+	public AbstractFunctionCall getEmbeddedFunctionCall() {
+		return e.getEmbeddedFunctionCall();
+	}
+
+	@Override
+	public AbstractExpression replaceEmbeddedFunctionCall(AbstractFunctionCall fc, String var) {
+		return new Peek((AbstractExpression)e.replaceEmbeddedFunctionCall(fc, var));
+	}
+
+	@Override
+	public Object getValue(VarStore e) throws RPLException {
+		return this.e.getValue(e, Type.STACK).peek();
+	}
+
+	@Override
+	public boolean hasDefiniteValue() {
+		return e.hasDefiniteValue();
+	}
+
+	@Override
+	public Object getDefiniteValue() throws RPLException {
+		return this.e.getDefiniteValue(Type.STACK).peek();
+	}
+
+	public String toString() {
+		return "peek("+e+")";
+	}
+	
+	public boolean equals(Object o) {
+		return (o instanceof Peek) && ((Peek)o).e.equals(e);
+	}
+	
+	@Override
+	public int hashCode() {
+		return e.hashCode();
+	}
+
+}
---
+++ src/main/java/com/tr/rp/ast/expressions/PopFunction.java
@@ -0,0 +1,85 @@
+package com.tr.rp.ast.expressions;
+
+import java.util.function.Consumer;
+
+import com.tr.rp.ast.AbstractExpression;
+import com.tr.rp.ast.LanguageElement;
+import com.tr.rp.exceptions.RPLException;
+import com.tr.rp.iterators.ranked.ExecutionContext;
+import com.tr.rp.iterators.ranked.RankedIterator;
+import com.tr.rp.varstore.VarStore;
+import com.tr.rp.varstore.types.Type;
+
+public class PopFunction extends AbstractFunctionCall {
+
+	private final String stackVar;
+	
+	public PopFunction(String stackVar) {
+		this.stackVar = stackVar;
+	}
+	
+	@Override
+	public LanguageElement replaceVariable(String a, String b) {
+		return this;
+	}
+
+	@Override
+	public String toString() {
+		return "pop("+stackVar+")";
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (o instanceof PopFunction) {
+			return ((PopFunction)o).stackVar.equals(stackVar);
+		}
+		return false;
+	}
+
+	@Override
+	public RankedIterator<VarStore> getIterator(ExecutionContext c, String assignToVar,
+			RankedIterator<VarStore> parent) throws RPLException {
+		return new RankedIterator<VarStore>() {
+
+			private Object poppedValue;
+			
+			private void setPoppedValue(Object value) {
+				this.poppedValue = value;
+			}
+			
+			private Object getPoppedValue() {
+				return poppedValue;
+			}
+			
+			@Override
+			public boolean next() throws RPLException {
+				return parent.next();
+			}
+
+			@Override
+			public VarStore getItem() throws RPLException {
+				VarStore v = parent.getItem();
+				v = v.create(stackVar, v.getValue(stackVar, Type.STACK).pop(this::setPoppedValue));
+				v = v.create(assignToVar, getPoppedValue());
+				return v;
+			}
+
+			@Override
+			public int getRank() {
+				return parent.getRank();
+			}
+			
+		};
+	}
+
+	@Override
+	public AbstractExpression transformRankExpressions(VarStore v, int rank) throws RPLException {
+		return this;
+	}
+
+	@Override
+	public AbstractExpression replaceEmbeddedFunctionCall(AbstractFunctionCall fc, String var) {
+		return this;
+	}
+
+}

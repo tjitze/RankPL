3dc8e271552dee929623173136305a927dec88d8
Tjitze <tjitze@gmail.com>
2017-10-14 01:30:18 +0200

    Clean up imports

--- src/main/java/com/tr/rp/varstore/TrieMapVarStore.java
+++ src/main/java/com/tr/rp/varstore/TrieMapVarStore.java
@@ -1,8 +1,6 @@
 package com.tr.rp.varstore;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Objects;
 import java.util.stream.Collectors;
 
 import com.google.common.collect.ImmutableList;

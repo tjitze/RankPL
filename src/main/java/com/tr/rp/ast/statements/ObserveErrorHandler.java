72ab209087e7623f5cd68c4bd4bf0d4b53b45a6a
Tjitze <tjitze@gmail.com>
2017-10-18 00:23:24 +0200

    Remove unused interface

--- src/main/java/com/tr/rp/ast/statements/ObserveErrorHandler.java
+++
@@ -1,9 +0,0 @@
-package com.tr.rp.ast.statements;
-
-import com.tr.rp.exceptions.RPLException;
-
-public interface ObserveErrorHandler {
-
-	public void observeConditionError(RPLException e) throws RPLException;
-
-}
